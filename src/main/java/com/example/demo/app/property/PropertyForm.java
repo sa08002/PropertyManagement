0371b8488a99c4486deeda3bdf4ed3115956ac43
sa08002 <1078sa08002@gmail.com>
2021-10-14 07:54:03 +0900

    PropertyFormの実装

---
+++ src/main/java/com/example/demo/app/property/PropertyForm.java
@@ -0,0 +1,82 @@
+package com.example.demo.app.property;
+
+public class PropertyForm {
+	
+	private String property_name;
+	private String postal_code;
+	private String address;
+	private String tel1;
+	private String tel12;
+	private String fax;
+	private String email;
+	private String sales_office;
+	private String detail1;
+	private String detail2;
+	
+	public PropertyForm() {
+	}
+	
+	public String getProperty_name() {
+		return property_name;
+	}
+	public void setProperty_name(String property_name) {
+		this.property_name = property_name;
+	}
+	public String getPostal_code() {
+		return postal_code;
+	}
+	public void setPostal_code(String postal_code) {
+		this.postal_code = postal_code;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel1() {
+		return tel1;
+	}
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+	public String getTel12() {
+		return tel12;
+	}
+	public void setTel12(String tel12) {
+		this.tel12 = tel12;
+	}
+	public String getFax() {
+		return fax;
+	}
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSales_office() {
+		return sales_office;
+	}
+	public void setSales_office(String sales_office) {
+		this.sales_office = sales_office;
+	}
+	public String getDetail1() {
+		return detail1;
+	}
+	public void setDetail1(String detail1) {
+		this.detail1 = detail1;
+	}
+	public String getDetail2() {
+		return detail2;
+	}
+	public void setDetail2(String detail2) {
+		this.detail2 = detail2;
+	}
+
+
+
+}
