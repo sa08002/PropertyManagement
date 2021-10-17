899b5484902fccb545a81788724911462c6ce0a4
sa08002 <1078sa08002@gmail.com>
2021-10-17 13:45:48 +0900

    Controllerにて、confirmの実装

--- src/main/java/com/example/demo/app/property/PropertyController.java
+++ src/main/java/com/example/demo/app/property/PropertyController.java
@@ -2,7 +2,10 @@ package com.example.demo.app.property;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PropertyController {
 	
 	@GetMapping("/form")
-	public String form(Model model) {
+	public String form(PropertyForm propertyForm, Model model) {
 		model.addAttribute("title", "Property Form");
 		return "property/form";
+	}
+	
+	@PostMapping("/confirm")
+	public String confirm(@Validated PropertyForm propertyForm,
+			BindingResult result,
+			Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Property Form");
+			return "property/form";
+		}
+		model.addAttribute("title", "Confirm Page");
+		return "property/confirm";
 	}
 
 }
