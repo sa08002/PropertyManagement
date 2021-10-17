2716b6dcf1006d81d6d0224b7036167d5a7b327a
sa08002 <1078sa08002@gmail.com>
2021-10-17 12:53:09 +0900

    build.graidleの修正、PropertyFormの作成

--- src/main/java/com/example/demo/app/property/PropertyForm.java
+++ src/main/java/com/example/demo/app/property/PropertyForm.java
@@ -1,82 +1,111 @@
 package com.example.demo.app.property;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PropertyForm {
 	
-	private String property_name;
-	private String postal_code;
+	@NotNull
+	private String propertyName;
+	@Max(10)
+	private String postalCode;
+	@NotNull
 	private String address;
+	@NotNull
+	@Max(15)
 	private String tel1;
 	private String tel12;
+	@Max(15)
 	private String fax;
+	@Email(message = "Invalid E-mail Format")
 	private String email;
-	private String sales_office;
+	@NotNull
+	private String salesOffice;
 	private String detail1;
 	private String detail2;
 	
 	public PropertyForm() {
 	}
-	
-	public String getProperty_name() {
-		return property_name;
+
+	public String getPropertyName() {
+		return propertyName;
 	}
-	public void setProperty_name(String property_name) {
-		this.property_name = property_name;
+
+	public void setPropertyName(String propertyName) {
+		this.propertyName = propertyName;
 	}
-	public String getPostal_code() {
-		return postal_code;
+
+	public String getPostalCode() {
+		return postalCode;
 	}
-	public void setPostal_code(String postal_code) {
-		this.postal_code = postal_code;
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getTel1() {
 		return tel1;
 	}
+
 	public void setTel1(String tel1) {
 		this.tel1 = tel1;
 	}
+
 	public String getTel12() {
 		return tel12;
 	}
+
 	public void setTel12(String tel12) {
 		this.tel12 = tel12;
 	}
+
 	public String getFax() {
 		return fax;
 	}
+
 	public void setFax(String fax) {
 		this.fax = fax;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getSales_office() {
-		return sales_office;
+
+	public String getSalesOffice() {
+		return salesOffice;
 	}
-	public void setSales_office(String sales_office) {
-		this.sales_office = sales_office;
+
+	public void setSalesOffice(String salesOffice) {
+		this.salesOffice = salesOffice;
 	}
+
 	public String getDetail1() {
 		return detail1;
 	}
+
 	public void setDetail1(String detail1) {
 		this.detail1 = detail1;
 	}
+
 	public String getDetail2() {
 		return detail2;
 	}
+
 	public void setDetail2(String detail2) {
 		this.detail2 = detail2;
 	}
-
-
-
+	
 }
