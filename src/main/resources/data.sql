b04f33e91dfbd9e181df60840b300b06b4e87346
sa08002 <1078sa08002@gmail.com>
2021-11-27 13:12:03 +0900

    data.sql、detailカラムの削除

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO property(property_name, address, tel1, email, detail1, created)
-VALUES('アンチョビットゲームズ', '東京県アンチョビ3丁目', '99999999999', 'aaa@exampl.com', 'あああああ', '2021-10-09 15:10:52');
-INSERT INTO property(property_name, address, tel1, email, detail1, created)
-VALUES('モズク農園', '東京県モズク農園2丁目', '99999999999', 'aaa@exampl.com', 'あああああ', '2021-10-09 15:10:52');
-INSERT INTO property(property_name, address, tel1, email, detail1, created)
-VALUES('ガンガゼ美術館', '東京県ガンガゼ美術館4丁目', '99999999999', 'aaa@exampl.com', 'あああああ', '2021-10-09 15:10:52');
+INSERT INTO property(property_name, address, tel1, email, created)
+VALUES('アンチョビットゲームズ', '東京県アンチョビ3丁目', '99999999999', 'aaa@exampl.com', '2021-10-09 15:10:52');
+INSERT INTO property(property_name, address, tel1, email, created)
+VALUES('モズク農園', '東京県モズク農園2丁目', '99999999999', 'aaa@exampl.com', '2021-10-09 15:10:52');
+INSERT INTO property(property_name, address, tel1, email, created)
+VALUES('ガンガゼ美術館', '東京県ガンガゼ美術館4丁目', '99999999999', 'aaa@exampl.com', '2021-10-09 15:10:52');
